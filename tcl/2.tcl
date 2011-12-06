7d7881054e32442557876164ccdde3ffff04ab4d
Will Gretz <Will@WillGretz.com>
2011-12-05 17:51:14 -0800

    problem 2 solved

--- tcl/2.tcl
+++ tcl/2.tcl
@@ -5,4 +5,8 @@ while {$x < 4000000} {
   set seeds [linsert $seeds end $x]
   set x [next_fib_number $seeds]
 }
-puts $seeds
+set v 0
+foreach x $seeds {
+  if {$x % 2 == 0} { set v [expr $x+$v]}
+}
+puts $v
